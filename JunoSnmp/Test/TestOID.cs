4fde538a51b9d3872bcb2e5cad8d98e0dd066f2b
Jeremy Gibbons <jeremy.gibbons@laposte.net>
2016-08-24 06:28:05 +0200

    Fixed typo in file headers

--- JunoSnmp/Test/TestOID.cs
+++ JunoSnmp/Test/TestOID.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="TestBER.cs" company="None">
+﻿// <copyright file="TestOID.cs" company="None">
 //    <para>
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
